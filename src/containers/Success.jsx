bc5de60aeb31acffbc90c6ab1d4a66093ea35f2d
christian <cristhho@gmail.com>
2020-11-24 15:37:32 -0500

    google maps integration

--- src/containers/Success.jsx
+++ src/containers/Success.jsx
@@ -1,15 +1,24 @@
-import React from 'react'
+import React, { useContext } from 'react'
 
+import AppContext from '../context/appContext'
+import Map from '../components/Map';
+import useGoogleAddress from '../hooks/useGoogleAddress'
 import '../styles/components/Success.css';
 
 const Success = () => {
+  const {state} = useContext(AppContext)
+  const {buyer} = state
+  let address = 'platzi'
+  if(buyer.length > 0)
+    address = buyer[0].address
+  const location = useGoogleAddress(address)
   return (
     <div className="Success">
       <div className="Success-content">
-        <h2>Oscar, Gracias por tu compra</h2>
+        <h2>{`${buyer.name}, Gracias por tu compra`}</h2>
         <span>Tu pedido lelgara en 3 dias a tu direccion:</span>
         <div className="Success-map">
-          Google Maps
+          <Map data={location} />
         </div>
       </div>
     </div>
